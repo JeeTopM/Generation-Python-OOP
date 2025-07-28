28608f2a1b06bb2071f7c40bf293b9cd0e92bccc
Лев Романович Панас <jieba2005@list.ru>
2025-07-28 17:22:24 +0300

    start task 3

---
+++ 4. Attributes, properties, and methods/4.3 Instance Methods. Part 2/3. Class House.py
@@ -0,0 +1,32 @@
+"""
+Класс House
+
+Вам доступен класс House, описывающий дом. При создании экземпляра класс принимает два аргумента в следующем порядке:
+    color — цвет дома
+    rooms — количество комнат в доме
+Экземпляр данного класса имеет два атрибута:
+    color — цвет дома
+    rooms — количество комнат в доме
+Реализуйте для класса House два метода экземпляра:
+    paint() — метод, принимающий в качестве аргумента значение new_color и изменяющий текущий цвет дома на new_color
+    add_rooms() — метод, принимающий в качестве аргумента целое число n и увеличивающий количество комнат в доме на n
+"""
+
+
+class House:
+    def __init__(self, color, rooms):
+        self.color = color
+        self.rooms = rooms
+
+
+# тест 1
+house = House("white", 4)
+print(house.color)  # white
+print(house.rooms)  # 4
+
+# тест 2
+house = House("white", 4)
+house.paint("black")
+house.add_rooms(1)
+print(house.color)  # black
+print(house.rooms)  # 5
