03a75398e1b0029303b83438e50fa98f8179f57c
Лев Романович Панас <jieba2005@list.ru>
2025-07-28 17:31:11 +0300

    start task 4

---
+++ 4. Attributes, properties, and methods/4.3 Instance Methods. Part 2/4. Class Circle.py
@@ -0,0 +1,25 @@
+"""
+Класс Circle
+
+Реализуйте класс Circle, описывающий круг. При создании экземпляра класс должен принимать один аргумент:
+    radius — радиус круга
+Экземпляр класса Circle должен иметь три атрибута:
+    radius — радиус круга
+    diameter — диаметр круга
+    area — площадь круга
+"""
+
+
+# Код:
+class Circle:
+    pass
+
+
+# тест 1
+circle = Circle(1)
+print(circle.radius)  # 1
+# тест 2
+circle = Circle(5)
+print(circle.radius)  # 5
+print(circle.diameter)  # 10
+print(circle.area)  # 78.53981633974483
