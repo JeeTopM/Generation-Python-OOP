5dfa02a06a98ffbfbec77c9f70e3369686a94fe9
Лев Романович Панас <jieba2005@list.ru>
2025-10-05 15:49:23 +0300

    end: class Vector

--- 5. Magic methods/5.2 Object String Representation/3. class Vector.py
+++ 5. Magic methods/5.2 Object String Representation/3. class Vector.py
@@ -9,6 +9,16 @@
 И следующее неформальное строковое представление:
     Вектор на плоскости с координатами (<координата x>, <координата y>)
 '''
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'Вектор на плоскости с координатами ({self.x}, {self.y})'
+
+    def __repr__(self):
+        return f'Vector({self.x}, {self.y})'
 
 # 1
 vector = Vector(1, 2)
