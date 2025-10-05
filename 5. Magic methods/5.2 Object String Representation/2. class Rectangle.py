dd16653b36452e00ab40022e023414e6e0050540
Лев Романович Панас <jieba2005@list.ru>
2025-10-05 15:33:55 +0300

    start: class Rectangle

---
+++ 5. Magic methods/5.2 Object String Representation/2. class Rectangle.py
@@ -0,0 +1,25 @@
+'''
+Класс Rectangle
+Вам доступен класс Rectangle, описывающий прямоугольник. При создании экземпляра класс принимает два аргумента в следующем порядке:
+    length — длина прямоугольника
+    width — ширина прямоугольника
+Реализуйте для экземпляров класса Rectangle следующее формальное и неформальное строковое представление:
+    Rectangle(<длина прямоугольника>, <ширина прямоугольника>)
+'''
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+# 1
+rectangle = Rectangle(1, 2)
+print(str(rectangle)) # Rectangle(1, 2)
+print(repr(rectangle)) # Rectangle(1, 2)
+
+# 2
+rectangle1 = Rectangle(1, 2)
+rectangle2 = Rectangle(3, 4)
+print(rectangle1) # Rectangle(1, 2)
+print(repr(rectangle2)) # Rectangle(3, 4)
+
+
