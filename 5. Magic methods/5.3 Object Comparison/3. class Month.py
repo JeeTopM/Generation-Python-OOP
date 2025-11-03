54145fde66f78e0ea7aef3860d30d46e0d50e182
Лев Романович Панас <jieba2005@list.ru>
2025-11-03 23:51:18 +0300

    finish 5.3 - 3

--- 5. Magic methods/5.3 Object Comparison/3. class Month.py
+++ 5. Magic methods/5.3 Object Comparison/3. class Month.py
@@ -15,10 +15,33 @@ Month объект считается больше другого Month объе
 Методы, реализующие операции сравнения, должны уметь сравнивать как два Month объекта между собой,
     так и Month объект с кортежем из двух чисел, представляющих год и месяц.
 '''
+from functools import total_ordering
 
+@total_ordering
 class Month:
-    pass
+    def __init__(self, year, month):
+        self.year = year
+        self.month = month
 
+    def __repr__(self):
+        return f'Month({self.year}, {self.month})'
+
+    def __str__(self):
+        return f'{self.year}-{self.month}'
+
+    def __eq__(self, other):
+        if isinstance(other, Month):
+            return (self.year, self.month) == (other.year, other.month)
+        if isinstance(other, tuple) and len(other) == 2:
+            return (self.year, self.month) == other
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Month):
+            return (self.year, self.month) < (other.year, other.month)
+        if isinstance(other, tuple) and len(other) == 2:
+            return (self.year, self.month) < other
+        return NotImplemented
 
 # test 1
 print(Month(1999, 12) == Month(1999, 12)) # True
