8c09c4aa71f73afaa7f4d00a6a07becf7dad88e3
Лев Романович Панас <jieba2005@list.ru>
2025-11-11 17:36:31 +0300

    task 1 done

--- 5. Magic methods/5.4 Unary Operators and Functions/1. class ReversibleString.py
+++ 5. Magic methods/5.4 Unary Operators and Functions/1. class ReversibleString.py
@@ -12,7 +12,10 @@
 class ReversibleString:
     def __init__(self, string):
         self.string = string
-
+    def __str__(self):
+        return self.string
+    def __neg__(self):
+        return ReversibleString(self.string[::-1])
 # test
 string = ReversibleString('python')
 print(string) # python
