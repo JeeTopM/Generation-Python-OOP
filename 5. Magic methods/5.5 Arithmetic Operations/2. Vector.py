0f0d055ace2b58619f485af31a18b3778903431e
Лев Романович Панас <jieba2005@list.ru>
2025-12-04 13:54:17 +0300

    5.5 - 2: end

--- 5. Magic methods/5.5 Arithmetic Operations/2. Vector.py
+++ 5. Magic methods/5.5 Arithmetic Operations/2. Vector.py
@@ -23,10 +23,39 @@
 Операция умножения должна быть выполнима независимо от порядка операндов,
     то есть должна быть возможность умножить как вектор на число, так и число на вектор.
 """
+
+
 class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __repr__(self):
+        return f'Vector({self.x}, {self.y})'
+
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+        return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        return NotImplemented
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(self.x / other, self.y / other)
+        return NotImplemented
+
 
 # tests
 # 1
