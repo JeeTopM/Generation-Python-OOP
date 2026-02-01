821931c780a8f385728097c28e8bcd3847915247
Лев Романович Панас <jieba2005@list.ru>
2026-02-01 22:56:14 +0300

    task 4 done

--- 5. Magic methods/5.6 Callables/4. QuadraticPolynomial.py
+++ 5. Magic methods/5.6 Callables/4. QuadraticPolynomial.py
@@ -9,16 +9,25 @@
 Экземпляр класса QuadraticPolynomial должен возвращать значение выражения
     ax2+bx+c, где a,b и c — коэффициенты квадратного трехчлена.
 """
+
+
 class QuadraticPolynomial:
-    pass
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def __call__(self, x):
+        return self.a * x ** 2 + self.b * x + self.c
+
 
 if __name__ == '__main__':
     func = QuadraticPolynomial(1, 2, 1)
 
-    print(func(1)) # 4
-    print(func(2)) # 9
+    print(func(1))  # 4
+    print(func(2))  # 9
 
     func = QuadraticPolynomial(1, 3, 4)
 
-    print(func(1)) # 8
-    print(func(2)) # 14
+    print(func(1))  # 8
+    print(func(2))  # 14
