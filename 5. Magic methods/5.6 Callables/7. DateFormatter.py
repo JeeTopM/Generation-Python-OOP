7256fb727f415b043bb9ecc8c907607ae9e10cd4
Лев Романович Панас <jieba2005@list.ru>
2026-02-02 15:17:18 +0300

    add task 7

---
+++ 5. Magic methods/5.6 Callables/7. DateFormatter.py
@@ -0,0 +1,34 @@
+"""
+Нередко в разных странах используются разные форматы дат. Рассмотрим часть из них:
+код страны	формат даты
+ru	DD.MM.YYYY
+us	MM-DD-YYYY
+ca	YYYY-MM-DD
+br	DD/MM/YYYY
+fr	DD.MM.YYYY
+pt	DD-MM-YYYY
+Реализуйте класс DateFormatter, экземпляры которого позволяют преобразовывать даты в формат определенной страны из таблицы выше.
+    При создании экземпляра класс должен принимать один аргумент:
+        country_code — код страны
+Экземпляр класса DateFormatter должен являться вызываемым объектом и принимать один аргумент:
+    d — дата (тип date)
+Экземпляр класса DateFormatter должен возвращать строку с датой d в формате страны с кодом country_code.
+"""
+
+
+class DateFormatter:
+    pass
+
+
+if __name__ == '__main__':
+    # test 1
+    ru_format = DateFormatter('ru')
+    print(ru_format(date(2022, 11, 7)))  # 07.11.2022
+
+    # test 2
+    us_format = DateFormatter('us')
+    print(us_format(date(2022, 11, 7)))  # 11-07-2022
+
+    # test 3
+    ca_format = DateFormatter('ca')
+    print(ca_format(date(2022, 11, 7)))  # 2022-11-07
