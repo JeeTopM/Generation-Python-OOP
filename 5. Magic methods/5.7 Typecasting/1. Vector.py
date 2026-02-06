09897f38ce6cd0aa92132b8a0d780b79feb1baa7
Лев Романович Панас <jieba2005@list.ru>
2026-02-06 13:46:01 +0300

    task 1 done

--- 5. Magic methods/5.7 Typecasting/1. Vector.py
+++ 5. Magic methods/5.7 Typecasting/1. Vector.py
@@ -16,8 +16,27 @@
 
 
 class Vector:
-    ...
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def __repr__(self):
+        return f'Vector({self.x}, {self.y})'
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __int__(self):
+        return int((self.x ** 2 + self.y ** 2) ** 0.5)
+
+    def __float__(self):
+        return float((self.x ** 2 + self.y ** 2) ** 0.5)
+
+    def __complex__(self):
+        return complex(self.x, self.y)
+
+    def __bool__(self):
+        return self.x != 0 or self.y != 0
 
 if __name__ == "__main__":
     # test 1
@@ -26,7 +45,7 @@ if __name__ == "__main__":
     print(vector)  # (3, 4)
     print(int(vector))  # 5
     print(float(vector))  # 5.0
-    print(complex(vector))  # (3 + 4j)
+    print(complex(vector))  # (3+4j)
 
     # test 2
     print(bool(Vector(1, 2)))  # True
