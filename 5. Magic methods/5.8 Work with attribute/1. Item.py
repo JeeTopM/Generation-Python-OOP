d2579d380880f91247d02e93d87f0925180e7ae6
Лев Романович Панас <jieba2005@list.ru>
2026-02-13 17:24:08 +0300

    start task 1

---
+++ 5. Magic methods/5.8 Work with attribute/1. Item.py
@@ -0,0 +1,40 @@
+'''
+Требовалось реализовать класс Item, описывающий предмет.
+При создании экземпляра класс должен был принимать три аргумента в следующем порядке:
+    name — название предмета
+    price — цена предмета в рублях
+    quantity — количество предметов
+Предполагалось, что при обращении к атрибуту name экземпляра класса Item будет возвращаться его название с заглавной буквы,
+    а при обращении к атрибуту total — произведение цены предмета на его количество.
+Программист торопился и решил задачу неправильно. Дополните приведенный ниже код и реализуйте правильный класс Item.
+'''
+
+
+class Item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __getattribute__(self, name):
+        if name == 'total':
+            return self.price * self.quantity
+        elif name == 'name':
+            return self.name.title()
+        return self.__dict__[name]
+
+
+if __name__ == '__main__':
+    fruit = Item('banana', 15, 5)
+    print(fruit.price)  # 15
+    print(fruit.quantity)  # 5
+
+    fruit = Item('banana', 15, 5)
+    print(fruit.name)  # Banana
+    print(fruit.total)  # 75
+
+    course = Item('pygen', 3900, 2)
+    print(course.name)  # Pygen
+    print(course.price)  # 3900
+    print(course.quantity)  # 2
+    print(course.total)  # 7800
