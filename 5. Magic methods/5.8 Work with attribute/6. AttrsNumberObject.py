74a060b0717508b534e8199a2c9e4de34ca74603
Лев Романович Панас <jieba2005@list.ru>
2026-02-17 18:04:58 +0300

    task 6: add

---
+++ 5. Magic methods/5.8 Work with attribute/6. AttrsNumberObject.py
@@ -0,0 +1,35 @@
+'''
+Реализуйте класс AttrsNumberObject.
+При создании экземпляра класс должен принимать произвольное количество именованных аргументов.
+Все передаваемые аргументы должны устанавливаться создаваемому экземпляру в качестве атрибутов.
+
+Экземпляр класса AttrsNumberObject должен иметь один атрибут:
+    attrs_num — количество атрибутов, которыми обладает экземпляр класса AttrsNumberObject на данный момент,
+        включая сам атрибут attrs_num
+'''
+
+
+class AttrsNumberObject:
+    ...
+
+
+if __name__ == '__main__':
+    # 1
+    music_group = AttrsNumberObject(name='Silent Poets', genre='acid jazz')
+    print(music_group.attrs_num)  # 3
+
+    # 2
+    music_group = AttrsNumberObject()
+    print(music_group.attrs_num)  # 1
+
+    # 3
+    music_group = AttrsNumberObject(name='Woodkid', genre='pop')
+    print(music_group.attrs_num)  # 3
+    music_group.country = 'France'
+    print(music_group.attrs_num)  # 4
+
+    # 4
+    music_group = AttrsNumberObject(name='Alexandra Savior', genre='dream pop')
+    print(music_group.attrs_num)  # 3
+    del music_group.genre
+    print(music_group.attrs_num)  # 2
